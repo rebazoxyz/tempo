608c94393557642777bc684416e26711d79acb19
Richard Janis Goldschmidt <github@aberrat.io>
2025-09-05 14:41:56 +0200

    xtask: add node subcmd to suggested command to run

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -186,7 +186,13 @@ fn generate_config(
     for (instance, (name, dst, cfg)) in configurations.iter().enumerate() {
         let eth_dst = cfg.storage_directory.with_file_name("reth_storage");
         let command = format!(
-            "cargo run --release --bin tempo-commonware -- \\\n--consensus-config {dst} \\\n--datadir {eth_dst} \\\n--instance {instance} \\\n--http"
+            "cargo run --release --bin tempo-commonware -- \
+                \\\nnode \
+                \\\n--consensus-config {dst} \
+                \\\n--datadir {eth_dst} \
+                \\\n--chain dev \
+                \\\n--instance {instance} \
+                \\\n--http"
         );
         println!("{name}: {command}");
     }
