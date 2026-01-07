e0931a404faacbaeb23f6b92c6d187f4f6285909
Alexey Shekhirin <github@shekhirin.com>
2026-01-07 20:09:59 +0000

    jemalloc-prof

--- bin/tempo/src/main.rs
+++ bin/tempo/src/main.rs
@@ -15,6 +15,13 @@
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
+#[global_allocator]
+static ALLOC: reth_cli_util::allocator::Allocator = reth_cli_util::allocator::new_allocator();
+
+#[cfg(all(feature = "jemalloc-prof", unix))]
+#[unsafe(export_name = "_rjem_malloc_conf")]
+static MALLOC_CONF: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19\0";
+
 mod defaults;
 
 use clap::Parser;
